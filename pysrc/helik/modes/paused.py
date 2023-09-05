f7fa25fef30b908d56fb9b1ba15c7f50dfff2db1
Marcin Bielewicz <marcin.bielewicz@gmail.com>
2023-09-05 13:37:57 +0200

    Fixed lang in GameModeType.PAUSED

--- pysrc/helik/modes/paused.py
+++ pysrc/helik/modes/paused.py
@@ -28,15 +28,15 @@ class ModePaused(Mode):
         self.res_man.get("surfaces", "buffer").blit(
             self.res_man.get(
                 "images", "default-background"), (0, 0))
-        label, rect = self.res_man.get_label(BoardType.GAME, "paused-shadow", "pl")
+        label, rect = self.res_man.get_label(BoardType.GAME, "paused-shadow", self.arena.lang)
         rect.center = (ARENA_WIDTH // 2, ARENA_HEIGHT // 2)
         rect.x += 5
         rect.y += 5
         self.res_man.get("surfaces", "buffer").blit(label, rect)
-        label, rect = self.res_man.get_label(BoardType.GAME, "paused", "pl")
+        label, rect = self.res_man.get_label(BoardType.GAME, "paused", self.arena.lang)
         rect.center = (ARENA_WIDTH // 2, ARENA_HEIGHT // 2)
         self.res_man.get("surfaces", "buffer").blit(label, rect)
-        label, rect = self.res_man.get_label(BoardType.GAME, "continue", "pl")
+        label, rect = self.res_man.get_label(BoardType.GAME, "continue", self.arena.lang)
         rect.center = (ARENA_WIDTH // 2, ARENA_HEIGHT // 2)
         rect.y += 100
         self.res_man.get("surfaces", "buffer").blit(label, rect)
