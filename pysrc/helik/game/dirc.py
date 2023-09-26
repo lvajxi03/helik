3729cab45e07828b2ec74b9b9f10a9a5e13e1916
Marcin Bielewicz <marcin.bielewicz@gmail.com>
2023-09-26 21:36:31 +0200

    DirC stub

--- pysrc/helik/game/dirc.py
+++ pysrc/helik/game/dirc.py
@@ -32,3 +32,37 @@ class DirChanger:
 
         self.w = self.rects[0].w
         self.h = self.rects[1].h
+        if self.x > ARENA_WIDTH:
+            self.visible = False
+        else:
+            self.visible = True
+        self.current = 0
+
+    def move(self, speed=1):
+        """
+        Move dir-changer according to its policy
+        """
+        self.x -= speed
+        if self.x + self.w < 0:
+            self.valid = False
+            self.visible = False
+        elif self.x < ARENA_WIDTH:
+            self.visible = True
+
+        self.current += 1
+        self.current %= len(self.images)
+
+    def on_paint(self, canvas):
+        """
+        Paint event handler
+        :param canvas: canvas to blit dir-changer image into
+        """
+        canvas.blit(self.images[self.current], (self.x, self.y))
+
+    def collide(self, other):
+        """
+        Check for collision between this object and the other
+        :param other: other object
+        :return tuple of intersection or None
+        """
+        return self.masks[self.current].overlap(other.mask, (other.x - self.x, other.y - self.y))
